36b60c692ea01cdf75da11dbbc9eecbf246e1804
Karthikeyan Ramasamy <karthikz@Karthikz-MacBook-Pro.local>
2017-06-17 11:55:03 -0700

    added package generation for pulsar spout for heron

--- scripts/release/status.sh
+++ scripts/release/status.sh
@@ -26,7 +26,7 @@ function disable_e_and_execute {
 }
 
 # get the release tag version or the branch name
-if [ -z ${HERON_BUILD_VERSION+x} ];
+if [ -z ${BUILD_VERSION+x} ];
 then
   cmd="git rev-parse --abbrev-ref HEAD"
   build_version=$($cmd) || die "Failed to run command to check head: $cmd"
@@ -37,55 +37,55 @@ then
     build_version=$($cmd) || die "Failed to run command to get git release: $cmd"
   fi
 else
-  build_version=${HERON_BUILD_VERSION}
+  build_version=${BUILD_VERSION}
 fi
-echo "HERON_BUILD_VERSION ${build_version}"
+echo "BUILD_VERSION ${build_version}"
 
 # The code below presents an implementation that works for git repository
-if [ -z ${HERON_GIT_REV+x} ];
+if [ -z ${GIT_REV+x} ];
 then
   cmd="git rev-parse HEAD"
   git_rev=$($cmd) || die "Failed to get git revision: $cmd"
 else
-  git_rev=${HERON_GIT_REV}
+  git_rev=${GIT_REV}
 fi
 
-echo "HERON_BUILD_SCM_REVISION ${git_rev}"
+echo "BUILD_SCM_REVISION ${git_rev}"
 
-if [ -z ${HERON_BUILD_HOST+x} ];
+if [ -z ${BUILD_HOST+x} ];
 then
   build_host=$(hostname)
 else
-  build_host=${HERON_BUILD_HOST}
+  build_host=${BUILD_HOST}
 fi
-echo "HERON_BUILD_HOST ${build_host}"
+echo "BUILD_HOST ${build_host}"
 
-if [ -z ${HERON_BUILD_TIME+x} ];
+if [ -z ${BUILD_TIME+x} ];
 then
   build_time=$(LC_ALL=en_EN.utf8 date)
 else
-  build_time=${HERON_BUILD_TIME}
+  build_time=${BUILD_TIME}
 fi
-echo "HERON_BUILD_TIME ${build_time}"
+echo "BUILD_TIME ${build_time}"
 
-if [ -z ${HERON_BUILD_TIMESTAMP+x} ];
+if [ -z ${BUILD_TIMESTAMP+x} ];
 then
   build_timestamp=$(date +%s000)
 else
-  build_timestamp=${HERON_BUILD_TIMESTAMP}
+  build_timestamp=${BUILD_TIMESTAMP}
 fi
-echo "HERON_BUILD_TIMESTAMP ${build_timestamp}"
+echo "BUILD_TIMESTAMP ${build_timestamp}"
 
-if [ -z ${HERON_BUILD_USER+x} ];
+if [ -z ${BUILD_USER+x} ];
 then
   build_user=${USER}
 else
-  build_user=${HERON_BUILD_USER}
+  build_user=${BUILD_USER}
 fi
-echo "HERON_BUILD_USER ${build_user}"
+echo "BUILD_USER ${build_user}"
 
 # Check whether there are any uncommited changes
-if [ -z ${HERON_TREE_STATUS+x} ];
+if [ -z ${TREE_STATUS+x} ];
 then
   status=$(disable_e_and_execute "git diff-index --quiet HEAD --")
   if [[ $status == 0 ]];
@@ -95,6 +95,6 @@ then
     tree_status="Modified"
   fi
 else
-  tree_status=${HERON_TREE_STATUS}
+  tree_status=${TREE_STATUS}
 fi
-echo "HERON_BUILD_RELEASE_STATUS ${tree_status}"
+echo "BUILD_RELEASE_STATUS ${tree_status}"
