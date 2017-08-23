1c3dcfaa316261688429ad39725a37630f6823ad
Chris <chris@Chriss-MacBook-Pro.local>
2017-08-23 12:19:15 -0700

    Change connectors repo structure.

--- heron/spouts/pulsar/src/python/__init__.py
+++
@@ -1,5 +0,0 @@
-'''Pulsar Spout's top level interfaces'''
-__all__ = ['pulsar_spout']
-
-# expose all interfaces
-from .pulsar_spout import PulsarSpout
--- third_party/java/Empty.java
+++
@@ -1,2 +0,0 @@
-// bazel java_binary rule requires src files
-// Empty.java act as this src file to build the binary
