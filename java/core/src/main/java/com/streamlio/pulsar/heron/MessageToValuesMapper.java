e0e7a1992732c43819133ef9654d676f7b55874b
Ali Ahmed <a.ahmed@streaml.io>
2017-08-28 15:32:24 -0700

    reorg code

---
+++ java/core/src/main/java/com/streamlio/pulsar/heron/MessageToValuesMapper.java
@@ -0,0 +1,38 @@
+//  Copyright 2017 Twitter. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+package com.streamlio.pulsar.heron;
+
+import com.twitter.heron.api.topology.OutputFieldsDeclarer;
+import com.twitter.heron.api.tuple.Values;
+import org.apache.pulsar.client.api.Message;
+
+import java.io.Serializable;
+
+public interface MessageToValuesMapper extends Serializable {
+
+  /**
+   * Convert {@link Message} to tuple values.
+   *
+   * @param message
+   * @return
+   */
+  Values toValues(Message message);
+
+  /**
+   * Declare the output schema for the spout.
+   *
+   * @param declarer
+   */
+  void declareOutputFields(OutputFieldsDeclarer declarer);
+}
--- java/heron/pulsar/src/test/java/PulsarSpoutTests.java
+++ java/heron/pulsar/src/test/java/PulsarSpoutTests.java
@@ -1,3 +1,5 @@
+package pulsar;
+
 //  Copyright 2017 Twitter. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,37 +13,24 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-package com.twitter.heron.connectors.pulsar;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.pulsar.client.api.Consumer;
-import org.apache.pulsar.client.api.ConsumerConfiguration;
-import org.apache.pulsar.client.api.Message;
-import org.apache.pulsar.client.api.MessageId;
-import org.apache.pulsar.client.api.PulsarClient;
-import org.apache.pulsar.client.api.PulsarClientException;
-import org.apache.pulsar.client.api.SubscriptionType;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-
+import com.streamlio.pulsar.heron.MessageToValuesMapper;
+import com.streamlio.pulsar.heron.PulsarSpout;
 import com.twitter.heron.api.spout.SpoutOutputCollector;
 import com.twitter.heron.api.topology.TopologyContext;
 import com.twitter.heron.api.tuple.Values;
+import org.apache.pulsar.client.api.*;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.anyString;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.Matchers.*;
+import static org.testng.Assert.assertEquals;
 
 public class PulsarSpoutTests {
 
@@ -56,53 +45,53 @@ public class PulsarSpoutTests {
   private TopologyContext mockTopologyContext;
   private SpoutOutputCollector mockSpoutOutputCollector;
 
-  @Before
+  @BeforeTest
   public void before() {
     mockMessageToValuesMapper = Mockito.mock(MessageToValuesMapper.class);
     mockTopologyContext = Mockito.mock(TopologyContext.class);
     mockSpoutOutputCollector = Mockito.mock(SpoutOutputCollector.class);
   }
 
-  @Test(expected = NullPointerException.class)
+  @Test(expectedExceptions = NullPointerException.class)
   public void testMissingServiceUrl() {
     new PulsarSpout.Builder()
-        .setTopic(TOPIC)
-        .setSubscription(SUBSCRIPTION)
-        .setMessageToValuesMapper(mockMessageToValuesMapper)
-        .build();
+            .setTopic(TOPIC)
+            .setSubscription(SUBSCRIPTION)
+            .setMessageToValuesMapper(mockMessageToValuesMapper)
+            .build();
   }
 
-  @Test(expected = NullPointerException.class)
+  @Test(expectedExceptions = NullPointerException.class)
   public void testMissingTopic() {
     new PulsarSpout.Builder()
-        .setServiceUrl(SERVICE_URL)
-        .setSubscription(SUBSCRIPTION)
-        .setMessageToValuesMapper(mockMessageToValuesMapper)
-        .build();
+            .setServiceUrl(SERVICE_URL)
+            .setSubscription(SUBSCRIPTION)
+            .setMessageToValuesMapper(mockMessageToValuesMapper)
+            .build();
   }
 
-  @Test(expected = NullPointerException.class)
+  @Test(expectedExceptions = NullPointerException.class)
   public void testMissingSubscription() {
     new PulsarSpout.Builder()
-        .setServiceUrl(SERVICE_URL)
-        .setTopic(TOPIC)
-        .setMessageToValuesMapper(mockMessageToValuesMapper)
-        .build();
+            .setServiceUrl(SERVICE_URL)
+            .setTopic(TOPIC)
+            .setMessageToValuesMapper(mockMessageToValuesMapper)
+            .build();
   }
 
-  @Test(expected = NullPointerException.class)
+  @Test(expectedExceptions = NullPointerException.class)
   public void testMissingMessageToValuesMapper() {
     new PulsarSpout.Builder()
-        .setServiceUrl(SERVICE_URL)
-        .setTopic(TOPIC)
-        .setSubscription(SUBSCRIPTION)
-        .build();
+            .setServiceUrl(SERVICE_URL)
+            .setTopic(TOPIC)
+            .setSubscription(SUBSCRIPTION)
+            .build();
   }
 
-  @Test(expected = RuntimeException.class)
+  @Test(expectedExceptions = RuntimeException.class)
   public void testFailedToSubscribe() throws Exception {
     final PulsarSpout spout =
-        create(createMessages().iterator(), true);
+            create(createMessages().iterator(), true);
     spout.open(emptyConfig, mockTopologyContext, mockSpoutOutputCollector);
   }
 
@@ -110,24 +99,24 @@ public class PulsarSpoutTests {
   public void testNextTupleEmitted() throws PulsarClientException {
     final Message message = createMockMessage();
     final PulsarSpout spout =
-        create(createMessages(message).iterator(), false);
+            create(createMessages(message).iterator(), false);
 
     final Values values = Mockito.mock(Values.class);
     Mockito.when(mockMessageToValuesMapper.toValues(any(Message.class)))
-        .thenReturn(values);
+            .thenReturn(values);
 
     spout.open(emptyConfig, mockTopologyContext, mockSpoutOutputCollector);
 
     spout.nextTuple();
     Mockito.verify(mockSpoutOutputCollector, Mockito.times(1))
-        .emit(values, message.getMessageId());
+            .emit(values, message.getMessageId());
   }
 
   @Test
   public void testAcknowledgeAsyncCalledOnAck() throws PulsarClientException {
     final Message message = createMockMessage();
     final PulsarSpout spout =
-        create(createMessages(message).iterator(), false);
+            create(createMessages(message).iterator(), false);
     spout.open(emptyConfig, mockTopologyContext, mockSpoutOutputCollector);
 
     spout.ack(message.getMessageId());
@@ -142,8 +131,8 @@ public class PulsarSpoutTests {
   }
 
   private PulsarSpout create(final Iterator<Message> messages,
-      final boolean failToSubscribe)
-      throws PulsarClientException {
+                             final boolean failToSubscribe)
+          throws PulsarClientException {
 
 
     final PulsarClient client = Mockito.mock(PulsarClient.class);
@@ -158,15 +147,15 @@ public class PulsarSpoutTests {
       }
     };
     Mockito.when(client.subscribe(anyString(), anyString(),
-        any(ConsumerConfiguration.class))).then(answer);
+            any(ConsumerConfiguration.class))).then(answer);
 
     final PulsarSpout spout =
-        Mockito.spy(new PulsarSpout.Builder()
-        .setServiceUrl(SERVICE_URL)
-        .setTopic(TOPIC)
-        .setSubscription(SUBSCRIPTION)
-        .setMessageToValuesMapper(mockMessageToValuesMapper)
-        .build());
+            Mockito.spy(new PulsarSpout.Builder()
+                    .setServiceUrl(SERVICE_URL)
+                    .setTopic(TOPIC)
+                    .setSubscription(SUBSCRIPTION)
+                    .setMessageToValuesMapper(mockMessageToValuesMapper)
+                    .build());
 
     Mockito.doReturn(client).when(spout).createClient();
 
@@ -174,7 +163,7 @@ public class PulsarSpoutTests {
   }
 
   private Consumer createConsumer(final Iterator<Message> messages)
-      throws PulsarClientException {
+          throws PulsarClientException {
     final Consumer consumer = Mockito.mock(Consumer.class);
 
     final Answer<Message> answer = new Answer<Message>() {
